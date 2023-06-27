4b1ac7b2eaea2fed18a452e133f67567e77319e9
vibhusings <vibhu.singh307@gmail.com>
2023-06-27 18:12:58 +0530

    Image uploader added.

---
+++ lib/screens/authentication/image_uploader.dart
@@ -0,0 +1,138 @@
+import 'dart:io';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class ImageUploader extends StatefulWidget {
+  @override
+  _ImageUploaderState createState() => _ImageUploaderState();
+}
+
+class _ImageUploaderState extends State<ImageUploader> {
+  List<File?> _selectedImages = [];
+
+  @override
+  void initState() {
+    super.initState();
+    Firebase.initializeApp();
+  }
+
+  Future<void> _uploadImage(File image) async {
+    final FirebaseFirestore firestore = FirebaseFirestore.instance;
+    User? currentUser = await FirebaseAuth.instance.currentUser;
+    String? userId = currentUser?.uid;
+    try {
+      final fileName = DateTime.now().millisecondsSinceEpoch.toString();
+      final Reference reference =
+      FirebaseStorage.instance.ref().child('images/$fileName.jpg');
+      final UploadTask uploadTask = reference.putFile(image);
+
+      final TaskSnapshot taskSnapshot = await uploadTask.whenComplete(() => null);
+      final String downloadURL = await taskSnapshot.ref.getDownloadURL();
+      if(_selectedImages.length == 0) {
+        await firestore.collection('users')
+        .doc(userId).collection('profile').doc('User_Images').set({
+          'MainImage': downloadURL,
+        });
+      }else if(_selectedImages.length == 1) {
+        await firestore.collection('users')
+            .doc(userId).collection('profile').doc('User_Images').set({
+          'Img2': downloadURL,
+
+        });
+      }else {
+        await firestore.collection('users')
+            .doc(userId).collection('profile').doc('User_Images').set({
+          'Img3': downloadURL,
+
+        });
+
+      }
+      print('Image uploaded. Download URL: $downloadURL');
+    } on FirebaseException catch (e) {
+      print('Error uploading image to Firebase: $e');
+    }
+  }
+
+  Future<void> _pickImage(ImageSource source) async {
+    final imagePicker = ImagePicker();
+    final pickedImage = await imagePicker.pickImage(source: source);
+
+    if (pickedImage != null) {
+      setState(() {
+        _selectedImages.add(File(pickedImage.path));
+      });
+    }
+  }
+
+  Widget _buildImagePreview(File? image) {
+    if (image == null) {
+      return Container();
+    }
+
+    return Container(
+      width: 150,
+      height: 150,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: FileImage(image),
+          fit: BoxFit.cover,
+        ),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      margin: EdgeInsets.symmetric(horizontal: 10),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('UMD Match'),
+        backgroundColor: Colors.red,
+      ),
+      body: Column(
+        children: [
+          SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () => _pickImage(ImageSource.camera),
+                child: Text('Take Photo'),
+              ),
+              SizedBox(width: 10),
+              ElevatedButton(
+                onPressed: () => _pickImage(ImageSource.gallery),
+                child: Text('Pick from Gallery'),
+              ),
+            ],
+          ),
+          SizedBox(height: 20),
+          Column(
+            children: _selectedImages
+                .map((image) => _buildImagePreview(image))
+                .toList(),
+          ),
+          SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {
+              if (_selectedImages.length == 3) {
+                for (final image in _selectedImages) {
+                  _uploadImage(image!);
+                }
+              } else {
+                print('Please select three images.');
+              }
+            },
+            child: Text('Upload Images'),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/screens/authentication/register.dart
+++ lib/screens/authentication/register.dart
@@ -2,14 +2,19 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:csv/csv.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:flutter/services.dart';
-import 'package:firebase_storage/firebase_storage.dart';
-import 'package:lib/Models/user.dart';
+//import 'package:firebase_storage/firebase_storage.dart';
+//import 'package:lib/Models/user.dart';
 import '../../Home/home.dart';
+//import '../../services/auth.dart';
 import 'auth.dart';
-import 'package:dropdown_search/dropdown_search.dart';
+import 'image_uploader.dart';
+//import 'auth.dart';
+//import 'email_verification_page.dart';
+//import 'package:dropdown_search/dropdown_search.dart';
 
 
 class Register extends StatefulWidget {
@@ -88,18 +93,26 @@ class _RegisterState extends State<Register> {
                 labelText: 'Name',
                 border: OutlineInputBorder(),
               ),
+              validator: (value){
+                if(value!.isEmpty) {
+                  return "Enter your name";
+                }else {
+                  return null;
+                }
+              },
             ),
             SizedBox(height: 16.0),
             Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
               const Spacer(),
               IconButton(
                 onPressed: () {
-                  //try {
-                  storeUserName(nameController.text);
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => DateofBirth()));
+                    storeUserName(nameController.text);
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => DateofBirth()));
+
+
                 },
-                        /*.then((value) async
+                /*.then((value) async
                     {
                       await FirebaseFirestore.instance.collection("users").doc(
                           value.user.uid).set({
@@ -127,21 +140,23 @@ class _RegisterState extends State<Register> {
 
 class DateofBirth extends StatelessWidget {
   final TextEditingController dateController = TextEditingController();
+  final TextEditingController yearController = TextEditingController();
+  final TextEditingController monthController = TextEditingController();
 
   Future<void> storeUserDOB(String dob) async {
     User? currentUser = await FirebaseAuth.instance.currentUser;
     String? userId = currentUser?.uid;
-       try {
-         DateTime dobDate = DateTime.parse(dob);
-         await FirebaseFirestore.instance
-             .collection('users')
-             .doc(userId).collection('profile').doc('required')
-             .set({'dob': Timestamp.fromDate(dobDate)}, SetOptions(merge: true));
-         print('User date of birth stored successfully.');
-       } catch (error) {
-         print('Failed to store user date of birth: $error');
-       }
-     }
+    try {
+      DateTime dobDate = DateTime.parse(dob);
+      await FirebaseFirestore.instance
+          .collection('users')
+          .doc(userId).collection('profile').doc('required')
+          .set({'dob': Timestamp.fromDate(dobDate)}, SetOptions(merge: true));
+      print('User date of birth stored successfully.');
+    } catch (error) {
+      print('Failed to store user date of birth: $error');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -163,11 +178,31 @@ class DateofBirth extends StatelessWidget {
             ),
             SizedBox(height: 16.0),
             TextFormField(
+              controller: yearController,
+              keyboardType: TextInputType.datetime,
+              decoration: InputDecoration(
+                labelText: 'Year',
+                hintText: 'YYYY',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 16.0),
+            TextFormField(
+              controller: monthController,
+              keyboardType: TextInputType.datetime,
+              decoration: InputDecoration(
+                labelText: 'Month',
+                hintText: 'MM',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 16.0),
+            TextFormField(
               controller: dateController,
               keyboardType: TextInputType.datetime,
               decoration: InputDecoration(
                 labelText: 'Date of Birth',
-                hintText: 'MM/DD/YYYY',
+                hintText: 'DD',
                 border: OutlineInputBorder(),
               ),
             ),
@@ -183,7 +218,8 @@ class DateofBirth extends StatelessWidget {
               ),
               IconButton(
                 onPressed: () {
-                  storeUserDOB(dateController.text);
+                  String dob = yearController.text + "/"+ monthController.text + "/" + dateController.text;
+                  storeUserDOB(dob);
                   Navigator.push(context,
                       MaterialPageRoute(builder: (context) => Major()));
                 },
@@ -265,21 +301,44 @@ class _MajorState extends State<Major> {
               style: TextStyle(fontSize: 24.0),
             ),
             SizedBox(height: 16.0),
-            DropdownButton<String>(
-              isExpanded: true,
-              value: dropDownVal,
-              hint: Text('Select Major'),
-              onChanged: (newValue) {
-                setState(() {
-                  dropDownVal = newValue!;
+            Autocomplete<String>(
+              optionsBuilder: (TextEditingValue textEditingValue) {
+                if (textEditingValue.text.isEmpty) {
+                  return const Iterable<String>.empty();
+                }
+                return _data.map((List<dynamic> row) {
+                  return row[0].toString();
+                }).where((String option) {
+                  return option.toLowerCase().contains(textEditingValue.text.toLowerCase());
                 });
               },
-              items: _data.map((List<dynamic> row) {
-                return DropdownMenuItem<String>(
-                  value: row[0].toString(),
-                  child: Text(row[0].toString()),
+              onSelected: (String selectedOption) {
+                setState(() {
+                  dropDownVal = selectedOption;
+                });
+              },
+              fieldViewBuilder: (BuildContext context, TextEditingController textEditingController, FocusNode focusNode, VoidCallback onFieldSubmitted) {
+                return TextFormField(
+                  controller: textEditingController,
+                  decoration: InputDecoration(
+                    labelText: 'Select Major',
+                  ),
                 );
-              }).toList(),
+              },
+              // isExpanded: true,
+              // value: dropDownVal,
+              // hint: Text('Select Major'),
+              // onChanged: (newValue) {
+              //   setState(() {
+              //     dropDownVal = newValue!;
+              //   });
+              // },
+              // items: _data.map((List<dynamic> row) {
+              //   return DropdownMenuItem<String>(
+              //     value: row[0].toString(),
+              //     child: Text(row[0].toString()),
+              //   );
+              // }).toList(),
             ),
             SizedBox(height: 16.0),
             Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
@@ -361,7 +420,7 @@ class _GenderState extends State<Gender> {
                 });
               },
               items:
-                  <String>['Male', 'Female', 'Nonbinary'].map((String value) {
+              <String>['Male', 'Female', 'Nonbinary'].map((String value) {
                 return DropdownMenuItem<String>(
                   value: value,
                   child: Text(value),
@@ -450,7 +509,7 @@ class _GenderPrefState extends State<GenderPref> {
                 });
               },
               items:
-                  <String>['Male', 'Female', 'Nonbinary'].map((String value) {
+              <String>['Male', 'Female', 'Nonbinary'].map((String value) {
                 return DropdownMenuItem<String>(
                   value: value,
                   child: Text(value),
@@ -473,7 +532,7 @@ class _GenderPrefState extends State<GenderPref> {
                   onPressed: () {
                     storeUserGenderPref(dropDownVal);
                     Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => Home()));
+                        MaterialPageRoute(builder: (context) => ImageUploader()));
                   },
                   icon: Icon(Icons.arrow_forward),
                   color: Colors.red,
@@ -487,3 +546,181 @@ class _GenderPrefState extends State<GenderPref> {
     );
   }
 }
+
+// class _uploadImg extends StatefulWidget {
+//   @override
+//   _uploadImgState createState() => _uploadImgState();
+// }
+//
+// class _uploadImgState extends State<_uploadImg> {
+//   Uint8List? _image;
+//   void selectImage() async {
+//     Uint8List img = await pickImage(ImageSource.gallery);
+//     setState(() {
+//       _image = img;
+//     });
+//   }
+//   void saveProfile() async{
+//     String resp = await StoreData().saveData(file: _image!);
+//   }
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text("UMD Match"),
+//       ),
+//       body: Center(
+//         child: Container(
+//           padding: const EdgeInsets.symmetric(
+//             horizontal: 32,
+//           ),
+//           child: Column(
+//             crossAxisAlignment: CrossAxisAlignment.center,
+//             children: [
+//               const SizedBox(
+//                 height: 24,
+//               ),
+//               Stack (
+//                 children: [
+//                   _image != null
+//                       ? CircleAvatar(
+//                     radius: 64,
+//                     backgroundImage: MemoryImage(_image!),
+//                   )
+//                       : const CircleAvatar(
+//                     radius: 64,
+//                     backgroundImage: NetworkImage(
+//                         'https://png.pngitem.com/pimgs/s/421-4212266_transparent-default-avatar-png-default-avatar-images-png.png'),
+//                   ),
+//                   Positioned(
+//                     bottom: -10,
+//                     left: 80,
+//                     child: IconButton(
+//                       onPressed: selectImage,
+//                       icon: const Icon(Icons.add_a_photo),
+//                     ),
+//                   )
+//                 ],
+//
+//               ),
+//               const SizedBox(
+//                 height: 24,
+//               ),
+//               ElevatedButton(
+//                 onPressed: saveProfile,
+//                 child: const Text('Save Profile'),
+//               )
+//
+//
+//             ],
+//           ),
+//         ),
+//
+//
+//       ),
+//     );
+//
+//   }
+// }
+
+
+
+// class UserImg extends StatefulWidget {
+//
+//   final Function(String imageUrl) onFileChanged;
+//   UserImg( {
+//     required this.onFileChanged,
+//   });
+//   @override
+//   _UserImgState createState() => _UserImgState();
+//
+//
+// }
+// class _UserImgState extends State<UserImg> {
+//   final ImagePicker _picker = ImagePicker();
+//
+//   String? imageUrl;
+//   @override
+//   Widget build(BuildContext context) {
+//     return Column(
+//       children: [
+//         if(imageUrl == null)
+//           Icon(Icons.image,size: 60,color: Theme.of(context).primaryColor),
+//         if(imageUrl != null)
+//           InkWell(
+//             splashColor: Colors.transparent,
+//             highlightColor: Colors.transparent,
+//             onTap: () => _selectPhoto(),
+//             child: AppRoundImage.url (
+//               imageUrl!,
+//               width:80,
+//               height:80,
+//             ),
+//           ),
+//         InkWell(
+//           onTap: () => _selectPhoto(context),
+//           child: Padding(
+//             padding: EdgeInsets.all(8.0),
+//             child: Text(imageUrl != null? 'Change Photo': 'Select Photo',
+//               style: TextStyle(color: Theme.of(context).primaryColor,fontWeight: FontWeight.bold),
+//             ),
+//           ),
+//         ),
+//
+//       ],
+//     );
+//   }
+// }
+// Future _selectPhoto(context) async {
+//   await showModalBottomSheet (context: context, builder: (context) => BottomSheet (
+//     builder: (context) => Column ( mainAxisSize: MainAxisSize.min, children: [
+//       ListTile(leading: Icon (Icons.camera), title: Text('Camera'), onTap: () {
+//         Navigator.of(context).pop();
+//         _pickImage (ImageSource .camera);
+//       }),
+//       ListTile(leading: Icon(Icons.filter), title: Text('Pick a file'), onTap: () {
+//         Navigator.of(context).pop();
+//         _pickImage(ImageSource.gallery);
+//       }),
+//     ],
+//     ),
+//     onClosing: () {}, )); // BottomSheet
+// }
+//
+//
+// Future _pickImage(ImageSource img) async {
+//   final pickedFile = await _picker.pickImage(source: source, imageQuality: 50);
+//   if(pickedFile == null) {
+//     return;
+//   }
+//   var file = await ImageCropper.cropImage(sourcePath: pickedFile.path, aspectRatio: CropAspectRatio(ratioX: 1, ratioY: 1) );
+//   if(file == null) {
+//     return;
+//   }
+//
+//   file = await compressImage(file.path,35) ;
+//   await _uploadFile(file.path);
+//
+// }
+//
+// Future compressImage(String path, int quality) async {
+//   final newPath = p.join((await getTemporaryDirectory()).path,'${DateTime.now()}.${p.extension(path)}');
+//
+//   final result = await FlutterImageCompress.compressAndGetFile(
+//     path, newPath, quality: quality,
+//   );
+//   return result;
+// }
+//
+// Future _uploadFile(String path) async {
+//   final ref = storage.FirebaseStorage.instance.ref().child ('images').child('${DateTime.now().toIso8601String() + p.basename (path)}');
+//
+//   final result = await ref.putFile(File(path));
+//   final fileUrl = await result.ref.getDownloadURL();
+//
+//   setState((){imageUrl = fileUrl;});
+//
+//   widget.onFileChanged(fileUrl);
+// }
+
+
